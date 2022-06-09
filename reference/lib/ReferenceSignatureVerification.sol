7680896a384d0d4607b728fc9bbd6fb0b5ea913e
Chomtana <Chomtana001@gmail.com>
2022-06-09 16:48:59 +0700

    Passed reference test

--- reference/lib/ReferenceSignatureVerification.sol
+++ reference/lib/ReferenceSignatureVerification.sol
@@ -20,9 +20,7 @@ contract ReferenceSignatureVerification is SignatureVerificationErrors {
      * @dev Internal view function to verify the signature of an order. An
      *      ERC-1271 fallback will be attempted if either the signature length
      *      is not 32 or 33 bytes or if the recovered signer does not match the
-     *      supplied signer. Note that in cases where a 32 or 33 byte signature
-     *      is supplied, only standard ECDSA signatures that recover to a
-     *      non-zero address are supported.
+     *      supplied signer.
      *
      * @param signer    The signer for the order.
      * @param digest    The digest to verify the signature against.
@@ -55,12 +53,14 @@ contract ReferenceSignatureVerification is SignatureVerificationErrors {
 
             // Ensure v value is properly formatted.
             if (v != 27 && v != 28) {
-                revert BadSignatureV(v);
+                // Revert with BadSignatureV(v) and passed the error to EIP-1271 signature verification.
+                bytes memory errorSignature = abi.encodeWithSignature("BadSignatureV(uint8)", v);
+                _assertValidEIP1271Signature(signer, digest, errorSignature.length, errorSignature, signature);
             }
         } else {
             // For all other signature lengths, try verification via EIP-1271.
             // Attempt EIP-1271 static call to signer in case it's a contract.
-            _assertValidEIP1271Signature(signer, digest, signature);
+            _assertValidEIP1271Signature(signer, digest, 0, "", signature);
 
             // Return early if the ERC-1271 signature check succeeded.
             return;
@@ -71,11 +71,14 @@ contract ReferenceSignatureVerification is SignatureVerificationErrors {
 
         // Disallow invalid signers.
         if (recoveredSigner == address(0)) {
-            revert InvalidSignature();
+            // Revert with InvalidSignature and passed the error to EIP-1271 signature verification.
+            bytes memory errorSignature = abi.encodeWithSignature("InvalidSignature()");
+            _assertValidEIP1271Signature(signer, digest, errorSignature.length, errorSignature, signature);
+            
             // Should a signer be recovered, but it doesn't match the signer...
         } else if (recoveredSigner != signer) {
             // Attempt EIP-1271 static call to signer in case it's a contract.
-            _assertValidEIP1271Signature(signer, digest, signature);
+            _assertValidEIP1271Signature(signer, digest, 0, "", signature);
         }
     }
 
@@ -91,13 +94,44 @@ contract ReferenceSignatureVerification is SignatureVerificationErrors {
     function _assertValidEIP1271Signature(
         address signer,
         bytes32 digest,
+        uint256 errorLength,
+        bytes memory errorSignature,
         bytes memory signature
     ) internal view {
+        bool isEOA;
+        assembly {
+            isEOA := iszero(extcodesize(signer))
+        }
+
+        if (isEOA) {
+            if (errorLength == 0) {
+                // If error is not passed from _assertValidSignature, revert with InvalidSigner().
+                revert InvalidSigner();
+            } else {
+                // If error is passed from _assertValidSignature, revert with passed error.
+                assembly {
+                    mstore(0, mload(add(errorSignature, 0x20)))
+                    mstore(0x04, mload(add(errorSignature, 0x24)))
+                    revert(0, errorLength)
+                }
+            }
+        }
+
         if (
             EIP1271Interface(signer).isValidSignature(digest, signature) !=
             EIP1271Interface.isValidSignature.selector
         ) {
-            revert InvalidSigner();
+            if (errorLength == 0) {
+                // If error is not passed from _assertValidSignature, revert with InvalidSigner().
+                revert InvalidSigner();
+            } else {
+                // If error is passed from _assertValidSignature, revert with passed error.
+                assembly {
+                    mstore(0, mload(add(errorSignature, 0x20)))
+                    mstore(0x04, mload(add(errorSignature, 0x24)))
+                    revert(0, errorLength)
+                }
+            }
         }
     }
 }
